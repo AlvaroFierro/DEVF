c65e4a21a8816997a6fca7f41532dafc26f8d64b
Alvaro Fierro <business.afierro@gmail.com>
2021-11-19 17:33:54 -0600

    Adding file

--- Mod_02/13_DOM/main.js
+++ Mod_02/13_DOM/main.js
@@ -1,1 +1,14 @@
 // DOM (Document Object Model)
+// Conecta a las paginas web a scripts o lenguajes de programacion.
+// Además es una representación completamente orientada al objeto de la página web y puede ser modificado con un lenguaje de script como JS
+
+// Usando getElementById
+// Lo que hace es obtener el elemento por nombre del Id
+
+document.getElementById("parrafo1").innerHTML =
+  "First paragraph made with the DOM";
+document.getElementById("parrafo2").innerHTML =
+  "Second paragraph made with the DOM";
+
+// Nota:
+// innerHTML es para cambiar el contenido del elemento
