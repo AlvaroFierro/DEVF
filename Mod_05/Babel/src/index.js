80e837b9800b7ce1fd3fa93fa688eaef1dd903ec
Alvaro Fierro <business.afierro@gmail.com>
2021-12-08 20:54:40 -0600

    Exersices done

--- Mod_05/Babel/src/index.js
+++ Mod_05/Babel/src/index.js
@@ -32,6 +32,35 @@ console.log(`El número invertido es: ${numeroInvertido(123)}`);
 
 // 2. Escribe una función JavaScript que genere todas las combinaciones de una cadena
 
+function combinator(s) {
+  combinations = [];
+  for (i = 0; i < s.length; i++) {
+    for (j = i + 1; j < s.length + 1; j++) {
+      combinations.push(s.slice(i, j));
+    }
+  }
+  return combinations;
+}
+console.log(combinator("dog"));
+
 // 3. Escribir una función JavaScript que acepte una cadena como parámetro y convierta la primera letra de cada palabra de la cadena en mayúscula
+function capitalize(str) {
+  return str
+    .split(" ")
+    .map((word) => word[0].toUpperCase() + word.slice(1))
+    .join(" ");
+}
+
+console.log(capitalize("Funcion que convierte la primer letra a mayuscula"));
 
 // 4. Escriba una función JavaScript que acepte una cadena como parámetro y encuentre la palabra más larga dentro de la cadena
+
+function largestWord(str) {
+  let words = str.split(" ");
+  let longestWord = words.reduce((longest, currentWord) => {
+    return currentWord.length > longest.length ? currentWord : longest;
+  }, "");
+  return longestWord;
+}
+
+console.log(largestWord("What's the longest word in this sentence"));
